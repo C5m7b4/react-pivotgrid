203e90942c89f9b79bb399162ebb6a39c0af99bd
C5m7b4 <c5m7b4@gmail.com>
2024-05-28 14:48:57 -0500

    adds draggable reorderable row fifelds

--- src/components/PivotTable/dropTargets/Rows.jsx
+++ src/components/PivotTable/dropTargets/Rows.jsx
@@ -1,19 +1,70 @@
+// import {useState} from 'react'
+import { doesRowExist } from "../../../utils/arrayUtils";
+
 const Rows = ({ rows, setRows }) => {
   const handleDrop = (e) => {
+    e.target.classList.remove("dragging");
     const fieldType = e.dataTransfer.getData("fieldType");
     const newRow = {
       label: fieldType,
       direction: "asc",
     };
-    const newRows = [...rows, newRow];
-    setRows(newRows);
+
+    const isExisting = doesRowExist(rows, "label", fieldType);
+    if (isExisting.found) {
+      const copy = rows.filter((r) => r.label != fieldType);
+      const afterElement = getDragAfterElement(
+        document.getElementById("filtered-rows"),
+        e.clientY
+      );
+
+      if (afterElement) {
+        const pos = rows.findIndex((r) => (r.label = afterElement.innerHTML));
+        copy.splice(pos, 0, newRow);
+        setRows(copy);
+      }
+    } else {
+      const newRows = [...rows, newRow];
+      setRows(newRows);
+    }
   };
 
   const handleDragOver = (e) => {
     e.preventDefault();
+    e.target.style.border = "2px solid #009879";
+    e.dataTransfer.dropEffect = "move";
   };
 
-  const handleDragLeave = () => {};
+  const handleDragStart = (e, fieldType) => {
+    e.dataTransfer.setData("fieldType", fieldType.label);
+    e.target.style.border = "1px solid #009879";
+    e.target.style.opacity = 0.8;
+    e.target.classList.add("dragging");
+  };
+
+  const handleDragLeave = (e) => {
+    e.target.style.border = null;
+  };
+
+  const getDragAfterElement = (container, y) => {
+    const draggableElements = [
+      ...container.querySelectorAll(".draggable-item:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        console.log(offset);
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+  };
   return (
     <>
       <div>Rows</div>
@@ -25,7 +76,15 @@ const Rows = ({ rows, setRows }) => {
         onDragLeave={handleDragLeave}
       >
         {rows.map((r, i) => (
-          <div key={`row-${i}`}>{r.label}</div>
+          <div
+            draggable
+            onDragStart={(e) => handleDragStart(e, r)}
+            onDragLeave={(e) => handleDragLeave(e)}
+            key={`row-${i}`}
+            className="draggable-item"
+          >
+            {r.label}
+          </div>
         ))}
       </div>
     </>
